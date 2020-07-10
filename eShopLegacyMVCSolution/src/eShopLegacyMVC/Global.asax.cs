1470cae6fd83a0a4fca090ff542e62f2042a7787
wawa0210 <xiaozhang0210@hotmail.com>
2020-07-10 10:19:55 +0800

    init

---
+++ eShopLegacyMVCSolution/src/eShopLegacyMVC/Global.asax.cs
@@ -0,0 +1,105 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using eShopLegacyMVC.Models;
+using eShopLegacyMVC.Models.Infrastructure;
+using eShopLegacyMVC.Modules;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace eShopLegacyMVC
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private static readonly ILog _log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        IContainer container;
+
+        protected void Application_Start()
+        {
+            container = RegisterContainer();
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ConfigDataBase();
+        }
+
+        /// <summary>
+        /// Track the machine name and the start time for the session inside the current session
+        /// </summary>
+        protected void Session_Start(Object sender, EventArgs e)
+        {
+            HttpContext.Current.Session["MachineName"] = Environment.MachineName;
+            HttpContext.Current.Session["SessionStartTime"] = DateTime.Now;
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            //set the property to our new object
+            LogicalThreadContext.Properties["activityid"] = new ActivityIdHelper();
+
+            LogicalThreadContext.Properties["requestinfo"] = new WebRequestInfo();
+
+            _log.Debug("WebApplication_BeginRequest");
+        }
+
+        /// <summary>
+        /// http://docs.autofac.org/en/latest/integration/mvc.html
+        /// </summary>
+        protected IContainer RegisterContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            var mockData = bool.Parse(ConfigurationManager.AppSettings["UseMockData"]);
+            builder.RegisterModule(new ApplicationModule(mockData));
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            return container;
+        }
+
+        private void ConfigDataBase()
+        {
+            var mockData = bool.Parse(ConfigurationManager.AppSettings["UseMockData"]);
+
+            if (!mockData)
+            {
+                Database.SetInitializer<CatalogDBContext>(container.Resolve<CatalogDBInitializer>());
+            }
+        }
+
+    }
+
+    public class ActivityIdHelper
+    {
+        public override string ToString()
+        {
+            if (Trace.CorrelationManager.ActivityId == Guid.Empty)
+            {
+                Trace.CorrelationManager.ActivityId = Guid.NewGuid();
+            }
+
+            return Trace.CorrelationManager.ActivityId.ToString();
+        }
+    }
+
+    public class WebRequestInfo
+    {
+        public override string ToString()
+        {
+            return HttpContext.Current?.Request?.RawUrl + ", " + HttpContext.Current?.Request?.UserAgent;
+        }
+    }
+}
